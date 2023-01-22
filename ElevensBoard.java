8e557acc3829cdc1c17c49d0af69f5a1893ebc8f
Mr. Gregson <agregson@utschools.ca>
2023-01-22 16:39:28 -0500

    Fixed Elevens board to maintain encapsulation

--- ElevensBoard.java
+++ ElevensBoard.java
@@ -32,131 +32,9 @@ public class ElevensBoard extends Board{
      */
     public ElevensBoard(){
         super(BOARD_SIZE);
-        cards = new Card[BOARD_SIZE];
-        deck = new Deck();
-        if (I_AM_DEBUGGING) {
-            System.out.println(deck);
-            System.out.println("----------");
-        }
-        dealMyCards();
     }
     
-    /**
-     * Start a new game by shuffling the deck and
-     * dealing some cards to this board.
-     */
-    public void newGame() {
-        deck.shuffle();
-        dealMyCards();
-    }
-
-
-    /**
-     * Accesses the size of the board.
-     * Note that this is not the number of cards it contains,
-     * which will be smaller near the end of a winning game.
-     * @return the size of the board
-     */
-    public int size() {
-        return cards.length;
-    }
-
-    /**
-     * Determines if the board is empty (has no cards).
-     * @return true if this board is empty; false otherwise.
-     */
-    public boolean isEmpty() {
-        for (int k = 0; k < cards.length; k++) {
-            if (cards[k] != null) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    /**
-     * Deal a card to the kth position in this board.
-     * If the deck is empty, the kth card is set to null.
-     * @param k the index of the card to be dealt.
-     */
-    public void deal(int k) {
-        cards[k] = deck.deal();
-    }
-
-    /**
-     * Accesses the deck's size.
-     * @return the number of undealt cards left in the deck.
-     */
-    public int deckSize() {
-        return deck.getSize();
-    }
-
-    /**
-     * Accesses a card on the board.
-     * @return the card at position k on the board.
-     * @param k is the board position of the card to return.
-     */
-    public Card cardAt(int k) {
-        return cards[k];
-    }
-
-    /**
-     * Replaces selected cards on the board by dealing new cards.
-     * @param selectedCards is a list of the indices of the
-     *        cards to be replaced.
-     */
-    public void replaceSelectedCards(List<Integer> selectedCards) {
-        for (Integer k : selectedCards) {
-            deal(k.intValue());
-        }
-    }
-
-    /**
-     * Gets the indexes of the actual (non-null) cards on the board.
-     *
-     * @return a List that contains the locations (indexes)
-     *         of the non-null entries on the board.
-     */
-    public List<Integer> cardIndexes() {
-        List<Integer> selected = new ArrayList<Integer>();
-        for (int k = 0; k < cards.length; k++) {
-            if (cards[k] != null) {
-                selected.add(k);
-            }
-        }
-        return selected;
-    }
-
-    /**
-     * Generates and returns a string representation of this board.
-     * @return the string version of this board.
-     */
-    public String toString() {
-        String s = "";
-        for (int k = 0; k < cards.length; k++) {
-            s = s + k + ": " + cards[k] + "\n";
-        }
-        return s;
-    }
-
-    /**
-     * Determine whether or not the game has been won,
-     * i.e. neither the board nor the deck has any more cards.
-     * @return true when the current game has been won;
-     *         false otherwise.
-     */
-    public boolean gameIsWon() {
-        if (deck.isEmpty()) {
-            for (Card c : cards) {
-                if (c != null) {
-                    return false;
-                }
-            }
-            return true;
-        }
-        return false;
-    }
-
+    
     /**
      * Determines if the selected cards form a valid group for removal.
      * In Elevens, the legal groups are (1) a pair of non-face cards
@@ -198,7 +76,6 @@ public class ElevensBoard extends Board{
             }
         }
         return false;
-        
     }
     
     /**
@@ -211,8 +88,8 @@ public class ElevensBoard extends Board{
      */
     private boolean containsPairSum11(List<Integer> selectedCards) {
         if (selectedCards.size() == 2){
-            Card card1 = cards[selectedCards.get(0)];
-            Card card2 = cards[selectedCards.get(1)];
+            Card card1 = cardAt(selectedCards.get(0));
+            Card card2 = cardAt(selectedCards.get(1));
             if (card1 == null || card2 == null)
                 return false;
             int pv1 = card1.getRank().getPointValue();
@@ -237,7 +114,7 @@ public class ElevensBoard extends Board{
             return false;
         Card[] cards = new Card[3];
         for (int i = 0; i < 3; ++i) {
-            cards[i] = this.cards[selectedCards.get(i)];
+            cards[i] = cardAt(selectedCards.get(i));
             if (cards[i] == null) return false;
         }
         Rank[] ranks = new Rank[3];
@@ -250,11 +127,5 @@ public class ElevensBoard extends Board{
                 return true; 
         }
         return false;
-    }
-    
-    private void dealMyCards() {
-        for (int k = 0; k < cards.length; k++) {
-            cards[k] = deck.deal();
-        }
     }
 }
